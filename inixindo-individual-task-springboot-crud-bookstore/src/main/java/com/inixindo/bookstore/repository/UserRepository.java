fa062c18dc3b24b808d5daa8f589a9898c5e4d7c
Adhito <45397405+Adhito@users.noreply.github.com>
2022-03-30 10:25:45 +0700

    Create Login/Authenticate Logic
    - Added login logic
    - Added registration logic

---
+++ inixindo-individual-task-springboot-crud-bookstore/src/main/java/com/inixindo/bookstore/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.inixindo.bookstore.repository;
+
+import com.inixindo.bookstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByUserName(String userName);
+}
